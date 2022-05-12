5f900d6a338ee6722df76758ad5e48ba8d446ce2
nolenzhao <n37zhao@uwaterloo>
2022-05-11 17:19:28 -0700

    commit 2

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,32 @@
 
-import ColourButton from './ColourButton'
+import ColourButton from './ColourButton';
+import {useState} from 'react';
+import camoshirttan from './assets/images/Camo-shirt-tan.png'
+import camoshirtcream from './assets/images/Camo-shirt-cream.png'
+
 const Item = (props) =>
 { 
+    let button_num = props.button_num;
+    let button_arr = []
+    
+   for(let i = 0; i < button_num; i++)
+   {
+        button_arr.push(<button onClick = {() => setclothescolour(props.imgsrc)} style = {{backgroundColor: props.color}} 
+        className = "actualbuttons"> test </button>)
+   }
+
+   const [clothescolour, setclothescolour] = useState(props.imginit)
+
     return(
      <div className = "itemcontainer">
         <div className = "clothesimage"> 
             <img src = {props.image} alt = {props.alt}/>
         </div>
         <div className = "colourbuttons">
-            <ColourButton/>
+           
+            
         </div>
-        <p className = "clothesdescription"> {props.description}</p> 
+        <p className = "clothesdescription"> {props.description}  </p> 
         
     </div>
     )
