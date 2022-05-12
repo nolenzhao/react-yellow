99777f2e2c25f2027d533dbea4a6fcd81bdd59ae
nolenzhao <n37zhao@uwaterloo>
2022-05-11 18:11:49 -0700

    button state colour

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -3,19 +3,20 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Titlecard from './Titlecard'
 import Item from './Item'
 import Jakescream from './assets/images/maskscream.png'
+import Camoshirttan from './assets/images/Camo-shirt-tan.png'
+import Camoshirtcream from './assets/images/Camo-shirt-cream.png'
 const Catalog = (props) =>
 {
     let itemarray = [];
-    for(let i = 0; i < 7; i++)
-    {
-        itemarray.push(<Item description = "description" image = {Jakescream}/>)
-    }
+    
 
     return (
         <div className = "content">
             <Titlecard title = "CATALOG"/>
             <div className = "catalogcontent">
-                {itemarray}
+                <Item description = "Camo shirt" button_num = {2} buttoncolour1 = '#b0947a' buttoncolour2 = "#e6dcd0"
+                buttonlink1 = {Camoshirttan} buttonlink2 = {Camoshirtcream} alt = "Camo Shirt" imginit = {Camoshirtcream}/>
+                
             </div>
             
         </div>
